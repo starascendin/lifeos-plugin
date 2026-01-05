7946527eb8020838cdebb3273db3af0d3e58a77e
Bryan Liu <star.ascendin@gmail.com>
2026-01-05 12:52:09 -0700

    cp -- dev

--- apps/lifeos/taurireact-macapp/src/components/lifeos/ProxyLLMCouncil.tsx
+++ apps/lifeos/taurireact-macapp/src/components/lifeos/ProxyLLMCouncil.tsx
@@ -5,7 +5,7 @@ export function LifeOSProxyLLMCouncil() {
     <AppShell>
       <div className="h-full w-full">
         <iframe
-          src="http://100.88.254.63:3456"
+          src="http://100.78.128.121:3456"
           className="h-full w-full border-0"
           title="Proxy LLM Council"
         />
